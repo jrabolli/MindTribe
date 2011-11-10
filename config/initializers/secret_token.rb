15a1cf7bdf4e2b71c4f7a00b1dff702bad406524
jason rabolli <jason.rabolli@gmail.com>
2011-11-09 21:04:16 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MindTribe::Application.config.secret_token = '4e9111b5ad526946c892fcc8a255f86554b25bcce446136b8d7d05169d03e4ac93d720734db995db5857cd78779672ab05787d7c3e61b7bca91a41a102daf3be'
