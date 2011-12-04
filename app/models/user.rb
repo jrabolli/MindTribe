3f79a0b9e253e83fef87507ca250daa8712bbe5d
jason rabolli <jason.rabolli@gmail.com>
2011-12-03 20:58:41 -0500

    gravatar and side bar

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,10 @@
 #  salt               :string(255)
 #
 
+require 'digest'
+
 class User < ActiveRecord::Base
+  attr_accessor :password
   attr_accessible :name, :email, :password, :password_confirmation
 
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
